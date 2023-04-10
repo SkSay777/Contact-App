f7c7fb15635e1f39276605103556ffc0e1034b4c
SkSayan <sislam_be21@thapar.edu>
2023-04-11 00:00:27 +0530

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactCard from "./ContactCard";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <AddContact/>
+      {/* <ContactCard/>
+      <ContactList/> */}
+    </div>
+  );
+}
+
+export default App;
